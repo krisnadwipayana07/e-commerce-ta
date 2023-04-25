93d185a0112ab6fa89410ab24e143621cc5412fc
AndreSuryana <Andresuryana17@gmail.com>
2023-04-25 22:33:54 +0800

    update year or month migration

--- database/migrations/2023_04_24_214339_add_column_year_or_month_in_submission_credit_transactions.php
+++ database/migrations/2023_04_24_214339_add_column_year_or_month_in_submission_credit_transactions.php
@@ -14,8 +14,8 @@ class AddColumnYearOrMonthInSubmissionCreditTransactions extends Migration
     public function up()
     {
         Schema::table('submission_credit_transactions', function (Blueprint $table) {
-            $table->string('year_or_month_occupied')->after('long_occupied');
-            $table->string('year_or_month_work')->after('length_of_work');
+            $table->string('year_or_month_occupied')->after('long_occupied')->default('Bulan')->comment('Bulan/Tahun');
+            $table->string('year_or_month_work')->after('length_of_work')->default('Bulan')->comment('Bulan/Tahun');
         });
     }
 

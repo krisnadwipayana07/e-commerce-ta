6b38c8c2363956f87137f598ccbb7d334bbf451b
krisnadwipayana07 <ikadekkrisnadwipayana@gmail.com>
2023-05-06 15:08:16 +0800

    Tahap 4 - Show detail transaction in credit paymen

--- resources/views/admin/submission_credit_payment/show.blade.php
+++ resources/views/admin/submission_credit_payment/show.blade.php
@@ -16,6 +16,29 @@
 <div class="border-bottom py-3">
     <h6 class="font-weight-bold">Remaining Instalment</h6>{{ ($data->transaction->credit_period - $data->transaction->total_phase) }}x
 </div>
+<div>
+    <h5 class="text-center mt-3">Detail Transaksi</h5>
+    <table class="table table-borderless">
+        <tbody>
+            @foreach ($data->transaction->transaction_detail as $item)
+                <tr>
+                    <td>
+                        {{ $item->property->name }}
+                    </td>
+                    <td>
+                        {{ format_rupiah($item->price) }}
+                    </td>
+                    <td> 
+                        x {{ $item->qty }}
+                    </td>
+                    <td>
+                        = {{ format_rupiah($item->total_price) }}
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 <div class="border-bottom py-3">
     <h6 class="font-weight-bold">Evidence Payment</h6>
     <img id="preview-ktp" src="{{ ($data->evidence_payment) ? url('/upload/customer/submission_credit_payment/', $data->evidence_payment) : 'https://cdn.pixabay.com/photo/2015/12/22/04/00/photo-1103595_960_720.png' }}" alt="preview image" style="height: 300px;">

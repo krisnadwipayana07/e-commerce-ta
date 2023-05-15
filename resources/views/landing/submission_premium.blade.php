9345173b2edfd6d2508dc182e70f3426193d09e0
krisnawijaksana <krisna.together0@gmail.com>
2023-05-15 23:16:49 +0800

    Premium

--- resources/views/landing/submission_premium.blade.php
+++ resources/views/landing/submission_premium.blade.php
@@ -6,7 +6,7 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="titlepage">
-                        <h2>Go To Premium</h2>
+                        <h2>Upgrade Akun Premium</h2>
                     </div>
                 </div>
             </div>
@@ -15,34 +15,34 @@
                     <form action="{{ route('customer.profile.submission_premium.store') }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         <div class="mb-3">
-                            <label for="ktp_name" class="form-label">Name</label>
+                            <label for="ktp_name" class="form-label">Nama Lengkap</label>
                             <input type="text" class="form-control" name="ktp_name" id="ktp_name">
                         </div>
                         <div class="mb-3">
-                            <label for="ktp_number" class="form-label">KTP Number</label>
+                            <label for="ktp_number" class="form-label">Nomor KTP</label>
                             <input type="text" class="form-control" name="ktp_number" id="ktp_number">
                         </div>
                         <div class="mb-3">
-                            <label for="ktp_address" class="form-label">KTP Address</label>
+                            <label for="ktp_address" class="form-label">Alamat Sesuai KTP</label>
                             <textarea name="ktp_address" id="ktp_address" cols="30" rows="10" placeholder="KTP Address" class="form-control"></textarea>
                         </div>
                         <div class="mb-3">
-                            <label for="salary" class="form-label">Salary</label>
+                            <label for="salary" class="form-label">Gaji (Pengahasilan)</label>
                             <input type="number" name="salary" id="salary" class="form-control">
                         </div>
                         <div class="mb-3">
-                            <label for="ktp" class="form-label">KTP</label>
+                            <label for="ktp" class="form-label">Foto KTP</label>
                             <input type="file" class="form-control" name="ktp" id="ktp">
                         </div>
                         <div class="mb-3">
-                            <label for="salary_slip" class="form-label">Slip Gaji</label>
+                            <label for="salary_slip" class="form-label">Foto Slip Gaji</label>
                             <input type="file" class="form-control" name="salary_slip" id="salary_slip">
                         </div>
                         <div class="mb-3">
-                            <label for="photo" class="form-label">Photo Selfie</label>
+                            <label for="photo" class="form-label">Foto Selfi</label>
                             <input type="file" class="form-control" name="photo" id="photo">
                         </div>
-                        <button type="submit" class="btn btn-success">Send</button>
+                        <button type="submit" class="btn btn-success">Kirim</button>
                     </form>
                 </div>
             </div>

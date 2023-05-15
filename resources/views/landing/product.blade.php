c303202bf7f5cc194057c2fa5ddeb844dc33f7d4
krisnadwipayana07 <ikadekkrisnadwipayana@gmail.com>
2023-05-15 20:59:55 +0800

    [tahap 5] update category product search

--- resources/views/landing/product.blade.php
+++ resources/views/landing/product.blade.php
@@ -14,24 +14,24 @@
     </div>
     <div id="project" class="project">
         <div class="container">
-            <div class="row">
-                <form action="{{ route('landing.product', $thisCategory->id) }}" method="GET">
+            <form action="{{ route('landing.product', $thisCategory->id) }}" method="GET">
+                <div class="row">
                     <div class="col-md-4">
-                        <div class="mb-3">
-                            <label for="sub_category" class="form-label">Select Sub Category To Filter</label>
+                        <label for="sub_category" class="form-label">Select Sub Category To Filter</label>
+                        <div class="d-flex">
                             <select id="sub_category" name="sub_category" class="form-select">
                                 <option value="">---SELECT---</option>
                                 @foreach ($subCategory as $sc)
-                                <option value="{{ $sc->id }}">{{ $sc->name }}</option>
+                                    <option value="{{ $sc->id }}">{{ $sc->name }}</option>
                                 @endforeach
                             </select>
+                            <button type="submit" class="btn btn-primary ml-1">
+                                <i class="ri-search-line"></i>
+                            </button>
                         </div>
                     </div>
-                    <div class="col-md-2">
-                        <button type="submit" class="btn btn-primary btn-block">Search</button>
-                    </div>
-                </form>
-            </div>
+                </div>
+            </form>
             <div class="row">
                 <div class="col-md-8">
 
@@ -60,12 +60,13 @@
                             <img src="{{ asset('upload/admin/property/' . $item->img) }}" alt="{{ asset($item->name) }}" />
                         </div>
                         <h3>
-                            <a href="{{ route('landing.detail', $item->id) }}" @if($item->stock < 1) style="pointer-events: none; cursor: default;" @endif>{{ $item->name }}</a>
+                            <a href="{{ route('landing.detail', $item->id) }}"
+                                @if ($item->stock < 1) style="pointer-events: none; cursor: default;" @endif>{{ $item->name }}</a>
                             <br><b style="color:brown">{{ format_rupiah($item->price) }}</b>
                             @if ($item->stock < 1)
-                            <br><b style="color:red">(Habis)</b>
+                                <br><b style="color:red">(Habis)</b>
                             @else
-                            <br><b style="color:black">Stock {{ $item->stock }}</b>
+                                <br><b style="color:black">Stock {{ $item->stock }}</b>
                             @endif
                         </h3>
                     </div>

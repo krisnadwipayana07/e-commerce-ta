aa86d68ca61fcc8398182f29cc4a287fa7189481
krisnadwipayana07 <ikadekkrisnadwipayana@gmail.com>
2023-05-15 14:03:11 +0800

    [tahap 5] rename cart to keranjang at cart page

--- resources/views/customer/cart/index.blade.php
+++ resources/views/customer/cart/index.blade.php
@@ -6,71 +6,74 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="titlepage">
-                        <h2>Cart</h2>
+                        <h2>Keranjang</h2>
                     </div>
                 </div>
             </div>
             @php
-            $total = 0;
+                $total = 0;
             @endphp
             <div class="row">
                 <div class="col-md-12 margin_top40">
                     @foreach ($carts as $cart)
-                    <div class="row">
-                        <div class="col-md-2">
-                            {{-- 
+                        <div class="row">
+                            <div class="col-md-2">
+                                {{-- 
                             <div class="align-middle">
                                 <input type="checkbox" class="checkbox" id="checkbox{{ $cart->id }}" value="{{ $cart->id }}">
                             </div>
                             --}}
-                        </div>
-                        <div class="col-md-2">
-                            <img src="{{ asset('upload/admin/property/' . $cart->property->img) }}"
-                                alt="{{ asset($cart->property->name) }}" />
-                        </div>
-                        <div class="col-md-6">
-                            <div class="my-3">
-                                @php
-                                $total += $cart->property->price * $cart->quantity;
-                                @endphp
-                                <b>{{ $cart->property->name }}</b>
-                                <br><b style="color:black">Jumlah&emsp;: {{ $cart->quantity }}x</b>
-                                <br><b style="color:black">Harga&emsp; : {{ format_rupiah($cart->property->price) }}</b>
-                                <br><h5><b style="color:brown">Total&emsp;: {{ format_rupiah($cart->property->price * $cart->quantity) }}</h5></b>
-                                <div class="row">
-                                    <div class="col-md-1">
-                                        <form action="{{ route('customer.cart.destroy', ['cart' => $cart->id]) }}" method="POST" class="mt-2">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger"><i class="ri-delete-bin-line"></i></button>
-                                        </form>
-                                    </div>
-                                    <div class="col-md-1">
-                                        <form class="mt-2">
-                                            <a href="{{ route('customer.cart.edit', ['cart' => $cart->id]) }}" class="btn btn-warning"><i class="fa fa-solid fa-pencil"></i></a>
-                                        </form>
+                            </div>
+                            <div class="col-md-2">
+                                <img src="{{ asset('upload/admin/property/' . $cart->property->img) }}"
+                                    alt="{{ asset($cart->property->name) }}" />
+                            </div>
+                            <div class="col-md-6">
+                                <div class="my-3">
+                                    @php
+                                        $total += $cart->property->price * $cart->quantity;
+                                    @endphp
+                                    <b>{{ $cart->property->name }}</b>
+                                    <br><b style="color:black">Jumlah&emsp;: {{ $cart->quantity }}x</b>
+                                    <br><b style="color:black">Harga&emsp; : {{ format_rupiah($cart->property->price) }}</b>
+                                    <br>
+                                    <h5><b style="color:brown">Total&emsp;:
+                                            {{ format_rupiah($cart->property->price * $cart->quantity) }}</h5></b>
+                                    <div class="row">
+                                        <div class="col-md-1">
+                                            <form action="{{ route('customer.cart.destroy', ['cart' => $cart->id]) }}"
+                                                method="POST" class="mt-2">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger"><i
+                                                        class="ri-delete-bin-line"></i></button>
+                                            </form>
+                                        </div>
+                                        <div class="col-md-1">
+                                            <form class="mt-2">
+                                                <a href="{{ route('customer.cart.edit', ['cart' => $cart->id]) }}"
+                                                    class="btn btn-warning"><i class="fa fa-solid fa-pencil"></i></a>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
                     @endforeach
                 </div>
             </div>
             <div class="row">
-                @if (count($carts)===0)
-                
-                <div class="text-center fw-bold py-4">Keranjang anda kosong, mohon isi terlebih dahulu </div>
+                @if (count($carts) === 0)
+                    <div class="text-center fw-bold py-4">Keranjang anda kosong, mohon isi terlebih dahulu </div>
                 @else
-                    
-                <div class="col-md-2"></div>
-                <div class="col-md-2"></div>
-                <div class="col-md-4">
-                    <!-- <b>Total: {{ format_rupiah($total) }}</b> -->
-                </div>
-                <div class="col-md-4">
-                    <a href="{{ route('customer.checkout.index') }}" class="btn btn-block btn-success">Checkout</a>
-                </div>
+                    <div class="col-md-2"></div>
+                    <div class="col-md-2"></div>
+                    <div class="col-md-4">
+                        <!-- <b>Total: {{ format_rupiah($total) }}</b> -->
+                    </div>
+                    <div class="col-md-4">
+                        <a href="{{ route('customer.checkout.index') }}" class="btn btn-block btn-success">Checkout</a>
+                    </div>
                 @endif
             </div>
         </div>

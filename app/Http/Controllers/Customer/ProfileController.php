a5f5fa175b774a395059fd12c1d0e94f584a0784
krisnadwipayana07 <ikadekkrisnadwipayana@gmail.com>
2023-05-15 23:22:10 +0800

    [tahap 5] update profile configuration

--- app/Http/Controllers/Customer/ProfileController.php
+++ app/Http/Controllers/Customer/ProfileController.php
@@ -18,34 +18,43 @@ use Illuminate\Support\Str;
 class ProfileController extends Controller
 {
     protected $category_product;
+    protected $gender = [
+        "Male" => "Laki - laki",
+        "Female" => "Wanita"
+    ];
     public function __construct()
     {
         $this->category_product = CategoryProperty::where('status', 'active')->get();
     }
-    
-    public function index() {
+
+    public function index()
+    {
         $user = Customer::where('id', auth()->guard('customer')->user()->id)->first();
+        $user->gender = $this->gender[$user->gender];
         $submission_premium = SubmissionPremiumCustomer::where("customer_id", $user->id)->get()->count();
         $isPremium = $submission_premium > 0 ? true : false;
         $header_category = $this->category_product;
         return view('landing.profile', compact('user', 'header_category', 'isPremium'));
     }
 
-    public function edit(Customer $customer) {
+    public function edit(Customer $customer)
+    {
         $header_category = $this->category_product;
         return view('landing.change_profile', compact('customer', 'header_category'));
     }
 
-    public function update(Request $request, Customer $customer) {
+    public function update(Request $request, Customer $customer)
+    {
         $request->validate(
             [
                 'username' => 'required|unique:customers,username,' . $customer->id,
-                'name' => 'required|unique:customers,name,'. $customer->id,
+                'name' => 'required|unique:customers,name,' . $customer->id,
                 'address' => 'required',
-                'phone_number' => 'required|unique:customers,phone_number,'. $customer->id,
-                'email' => 'required|unique:customers,email,'. $customer->id,
-                'myimg' =>'nullable|mimes:jpeg,png|max:2048',
-            ], [],
+                'phone_number' => 'required|unique:customers,phone_number,' . $customer->id,
+                'email' => 'required|unique:customers,email,' . $customer->id,
+                'myimg' => 'nullable|mimes:jpeg,png|max:2048',
+            ],
+            [],
             [
                 'username' => 'Username',
                 'name' => 'Name',
@@ -53,46 +62,50 @@ class ProfileController extends Controller
                 'phone_number' => 'Phone Number',
                 'email' => 'Email',
                 'myimg' => 'Image',
-            ]);
-            if($request->password || $request->password_confirmation){
-                $request->validate(
-                    [
-                        'password' => 'required|confirmed',
-                        'password_confirmation' => 'required',
-                    ], [],
-                    [
-                        'password' => 'Password',
-                        'password_confirmation' => 'Confirm Password',
-                    ]);
-            }
+            ]
+        );
+        if ($request->password || $request->password_confirmation) {
+            $request->validate(
+                [
+                    'password' => 'required|confirmed',
+                    'password_confirmation' => 'required',
+                ],
+                [],
+                [
+                    'password' => 'Password',
+                    'password_confirmation' => 'Confirm Password',
+                ]
+            );
+        }
 
         DB::beginTransaction();
-        try{
+        try {
             if ($request->hasFile('myimg')) {
                 $request['img'] = updateImg('upload/admin/customer/', $customer->img);
             }
-            
-            if($request->password && $request->password_confirmation){
+
+            if ($request->password && $request->password_confirmation) {
                 $customer->update($request->all());
-            }else{
+            } else {
                 $customer->update($request->except(['password']));
             }
             DB::commit();
-            return redirect()->route('customer.profile.index')->with('result', ['success', 'Data #'.$customer->name.' Updated Successfully.']);
-
-        }catch(Exception $ex){
+            return redirect()->route('customer.profile.index')->with('result', ['success', 'Data #' . $customer->name . ' Updated Successfully.']);
+        } catch (Exception $ex) {
             DB::rollback();
             return response()->json(['status' => 0, 'text' => 'Error Occur.']);
         }
     }
 
-    public function submission_premium_index() {
+    public function submission_premium_index()
+    {
         $user = Customer::where('id', auth()->guard('customer')->user()->id)->first();
         $header_category = $this->category_product;
         return view('landing.submission_premium', compact('user', 'header_category'));
     }
-    
-    public function submission_premium_store(Request $request) {
+
+    public function submission_premium_store(Request $request)
+    {
         DB::beginTransaction();
         try {
             $upload_path = "/upload/customer/submission_premium/";
@@ -100,25 +113,25 @@ class ProfileController extends Controller
             if (!File::isDirectory($path)) {
                 File::makeDirectory($path, 0777, true, true);
             }
-    
+
             if ($request->file('ktp')->isValid()) {
                 $ktp = $request->file('ktp');
-                $ktp_name = 'ktp_'. Str::random(5) . time() . '.' . $ktp->extension();
+                $ktp_name = 'ktp_' . Str::random(5) . time() . '.' . $ktp->extension();
                 $k = Image::make($ktp->path());
-                $k->save($path.''.$ktp_name);
+                $k->save($path . '' . $ktp_name);
             }
             if ($request->file('salary_slip')->isValid()) {
                 $salary_slip = $request->file('salary_slip');
-                $salary_slip_name = 'salaryslip_'. Str::random(5) . time() . '.' . $salary_slip->extension();
+                $salary_slip_name = 'salaryslip_' . Str::random(5) . time() . '.' . $salary_slip->extension();
                 $ss = Image::make($salary_slip->path());
-                $ss->save($path.''.$salary_slip_name);
+                $ss->save($path . '' . $salary_slip_name);
             }
-            
+
             if ($request->file('photo')->isValid()) {
                 $photo = $request->file('photo');
-                $photo_name = 'photo_'. Str::random(5) . time() . '.' . $photo->extension();
+                $photo_name = 'photo_' . Str::random(5) . time() . '.' . $photo->extension();
                 $p = Image::make($photo->path());
-                $p->save($path.''.$photo_name);
+                $p->save($path . '' . $photo_name);
             }
             SubmissionPremiumCustomer::create([
                 "customer_id" => auth()->guard('customer')->user()->id,
@@ -141,12 +154,14 @@ class ProfileController extends Controller
         }
     }
 
-    public function change_password_index(Customer $customer) {
+    public function change_password_index(Customer $customer)
+    {
         $header_category = $this->category_product;
         return view('landing.change_password', compact('customer', 'header_category'));
     }
 
-    public function change_password_update(Request $request, Customer $customer) {
+    public function change_password_update(Request $request, Customer $customer)
+    {
         DB::beginTransaction();
         try {
             $correct_password = Hash::check($request->old_password, $customer->password);

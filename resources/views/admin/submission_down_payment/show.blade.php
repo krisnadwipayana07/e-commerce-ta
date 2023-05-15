d610c39fa265acb17a3f39c7c3bf3a3ae76d6e77
krisnadwipayana07 <ikadekkrisnadwipayana@gmail.com>
2023-05-15 13:01:33 +0800

    [tahap 5] hide action button when approve

--- resources/views/admin/submission_down_payment/show.blade.php
+++ resources/views/admin/submission_down_payment/show.blade.php
@@ -25,7 +25,7 @@
                     <td>
                         {{ format_rupiah($item->price) }}
                     </td>
-                    <td> 
+                    <td>
                         x {{ $item->qty }}
                     </td>
                     <td>
@@ -48,25 +48,31 @@
 --}}
 <div class="border-bottom py-3">
     <h6 class="font-weight-bold">Evidence Payment</h6>
-    <img id="preview-ktp" src="{{ ($data->evidence_payment) ? url('/upload/customer/submission_dp_payment/', $data->evidence_payment) : 'https://cdn.pixabay.com/photo/2015/12/22/04/00/photo-1103595_960_720.png' }}" alt="preview image" style="height: 300px;">
-</div> 
-<div class="row">
-    <div class="col-md-2">
-        <form method="POST" action="{{ route('admin.submission.dp.payment.approve', $data->id) }}">
-            @csrf
-            @method('PUT')
-            <div class="border-bottom py-3">
-                <button type="submit" class="btn btn-success"><i class="fa fa-fw fa-paper-plane me-1"></i>Approve</button>
-            </div>
-        </form>
-    </div>
-    <div class="col-md-2">
-        <form method="POST" action="{{ route('admin.submission.dp.payment.reject', $data->id) }}">
-            @csrf
-            @method('PUT')
-            <div class="border-bottom py-3">
-                <button type="submit" class="btn btn-danger"><i class="fa fa-fw fa-paper-plane me-1"></i>Reject</button>
-            </div>
-        </form>
-    </div>
+    <img id="preview-ktp"
+        src="{{ $data->evidence_payment ? url('/upload/customer/submission_dp_payment/', $data->evidence_payment) : 'https://cdn.pixabay.com/photo/2015/12/22/04/00/photo-1103595_960_720.png' }}"
+        alt="preview image" style="height: 300px;">
 </div>
+@if ($data->status != 'accept')
+    <div class="row">
+        <div class="col-md-2">
+            <form method="POST" action="{{ route('admin.submission.dp.payment.approve', $data->id) }}">
+                @csrf
+                @method('PUT')
+                <div class="border-bottom py-3">
+                    <button type="submit" class="btn btn-success"><i
+                            class="fa fa-fw fa-paper-plane me-1"></i>Approve</button>
+                </div>
+            </form>
+        </div>
+        <div class="col-md-2">
+            <form method="POST" action="{{ route('admin.submission.dp.payment.reject', $data->id) }}">
+                @csrf
+                @method('PUT')
+                <div class="border-bottom py-3">
+                    <button type="submit" class="btn btn-danger"><i
+                            class="fa fa-fw fa-paper-plane me-1"></i>Reject</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endif
